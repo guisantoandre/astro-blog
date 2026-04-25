d56bbc63644f282b315b493ea62105a813bd2ba2
guilherme <gx2m042@gmail.com>
2026-04-25 16:13:25 -0300

    Ajustes MainLayout e página 404 adicionada

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = "Astroblog";
 export const SITE_DESCRIPTION = "Notícias e artigos sobre tecnologia";
+export const HOMEPAGE_ARTICLES_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
