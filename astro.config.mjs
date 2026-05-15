a89e86c57bcf875101ebeaf158b98ef3baa5b2a6
guilherme <gx2m042@gmail.com>
2026-05-15 10:52:02 -0300

    ajusta configuração de fontes para otimização

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -7,9 +7,68 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-     plugins: [tailwindcss()],
-  },
+   vite: {
+      plugins: [tailwindcss()],
+   },
 
-  adapter: vercel(),
+   adapter: vercel(),
+
+   fonts: [
+      {
+         provider: fontProviders.local(),
+         name: "Barlow",
+         cssVariable: "--font-sans",
+         options: {
+            variants: [
+               {
+                  src: ["./src/assets/fonts/Barlow-Regular.ttf"],
+                  weight: "400",
+                  style: "normal",
+               },
+            ],
+         },
+      },
+      {
+         provider: fontProviders.local(),
+         name: "Barlow",
+         cssVariable: "--font-sans",
+         options: {
+            variants: [
+               {
+                  src: ["./src/assets/fonts/Barlow-Light.ttf"],
+                  weight: "300",
+                  style: "normal",
+               },
+            ],
+         },
+      },
+      {
+         provider: fontProviders.local(),
+         name: "Barlow",
+         cssVariable: "--font-sans",
+         options: {
+            variants: [
+               {
+                  src: ["./src/assets/fonts/Barlow-SemiBold.ttf"],
+                  weight: "600",
+                  style: "normal",
+               },
+            ],
+         },
+      },
+      {
+         provider: fontProviders.local(),
+         name: "Orbitron",
+         cssVariable: "--font-display",
+         options: {
+            variants: [
+               {
+                  src: ["./src/assets/fonts/Orbitron-SemiBold.ttf"],
+                  weight: "600",
+                  style: "normal",
+               },
+            ],
+         },
+      },
+   ],
 });
